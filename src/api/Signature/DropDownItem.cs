2c74c0b1983b5ecfc2afe69d414efc8aed03e293
Chiefley <chief@thechief.com>
2025-03-28 11:08:01 -0400

    Api signature change and build fixes

---
+++ src/api/Signature/DropDownItem.cs
@@ -0,0 +1,7 @@
+namespace NCLims.Models.DTOs;
+
+public class DropDownItem
+{
+    public int Id { get; set; }
+    public string Label { get; set; }
+}
--- src/api/Signature/SopMaintenanceSelectors.cs
+++ src/api/Signature/SopMaintenanceSelectors.cs
@@ -4,8 +4,3 @@ public class SopMaintenanceSelectors
     public List<DropDownItem> PanelGroupItems { get; set; } = [];
 }
 
-public class DropDownItem
-{
-    public int? Id { get; set; }
-    public string Label { get; set; }
-}
