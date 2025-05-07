324bd069ec215249f3c4ea0f4485eb2dd6cc22dd
Chiefley <chief@thechief.com>
2025-05-07 19:52:44 -0400

    index barrel file

---
+++ src/models/types/index.ts
@@ -0,0 +1,10 @@
+// src/models/types/index.ts
+// This file re-exports all types from the new location for backward compatibility
+
+// Re-export all types from their new locations
+export * from '../../features/common/types/common';
+export * from '../../features/common/types/batchSops';
+export * from '../../features/prepBatchSop/types/prepBatchSops';
+export * from '../../features/analyticalBatchSop/types/analyticalBatchSops';
+export * from '../../features/labAssets/types/labAssets';
+export * from '../../features/basicTables/types/basicTables';
